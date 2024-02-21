371042b16639505e45a3837f61164679f5a58e9a
Ahmed Shaker <ashaker6573@gmail.com>
2024-02-21 14:25:46 +0200

    BloC and Denpendency injection

---
+++ lib/injection_container.dart
@@ -0,0 +1,51 @@
+import 'package:clean_architecture_posts_app/core/network/network_info.dart';
+import 'package:clean_architecture_posts_app/features/posts/data/datasources/post_local_data_source.dart';
+import 'package:clean_architecture_posts_app/features/posts/data/datasources/post_remote_data_source.dart';
+import 'package:clean_architecture_posts_app/features/posts/data/repositories/post_repository_impl.dart';
+import 'package:clean_architecture_posts_app/features/posts/domain/repositories/posts_repository.dart';
+import 'package:clean_architecture_posts_app/features/posts/domain/usecases/add_post.dart';
+import 'package:clean_architecture_posts_app/features/posts/domain/usecases/delete_post.dart';
+import 'package:clean_architecture_posts_app/features/posts/domain/usecases/get_all_posts.dart';
+import 'package:clean_architecture_posts_app/features/posts/domain/usecases/update_post.dart';
+import 'package:clean_architecture_posts_app/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'package:clean_architecture_posts_app/features/posts/presentation/bloc/posts/posts_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Bloc
+  sl.registerFactory(() => PostsBloc(getAllPosts: sl()));
+
+  sl.registerFactory(() => AddDeleteUpdatePostBloc(
+      addPost: sl(), deletePost: sl(), updatePost: sl()));
+
+  // Usecases
+  sl.registerLazySingleton(() => GetAllPostsUsecase(sl()));
+  sl.registerLazySingleton(() => AddPostUsecase(sl()));
+  sl.registerLazySingleton(() => DeletePostUsecase(sl()));
+  sl.registerLazySingleton(() => UpdatePostUsecase(sl()));
+
+  // Repositories
+  sl.registerLazySingleton<PostsRepository>(() => PostsRepsitoryImpl(
+      remoteDataSource: sl(), localDataSource: sl(), networkInfo: sl()));
+
+  // Datasources
+  sl.registerLazySingleton<PostRemoteDataSource>(
+      () => PostRemoteDataSourceImpl(client: sl()));
+  sl.registerLazySingleton<PostLocalDataSource>(
+      () => PostLocalDataSourceImpl(sharedPreferences: sl()));
+
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImp(sl()));
+
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,40 @@
 import 'package:clean_architecture_posts_app/core/app_theme.dart';
+import 'package:clean_architecture_posts_app/features/posts/presentation/bloc/add_delete_update_post/add_delete_update_post_bloc.dart';
+import 'package:clean_architecture_posts_app/features/posts/presentation/bloc/posts/posts_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'injection_container.dart' as di;
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appTheme,
-      title: 'Posts App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Posts'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => di.sl<PostsBloc>()),
+          BlocProvider(create: (_) => di.sl<AddDeleteUpdatePostBloc>()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: appTheme,
+          title: 'Posts App',
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Posts'),
+            ),
+            body: const Center(
+              child: Text('Hello World'),
+            ),
+          ),
+        ));
   }
 }
